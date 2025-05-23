eb538af7ec89f4d788d24540cdb3953380c3780a
leaox77 <leandroeguerdo@gmail.com>
2025-05-23 05:45:13 -0400

    cors

--- app/main.py
+++ app/main.py
@@ -16,7 +16,7 @@ app = FastAPI(
 # CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://sistema-de-inventarios-tu-super.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
